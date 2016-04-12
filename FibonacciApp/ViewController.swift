97c9ad922c715c0860d474234de7137c0843b0a8
Karlee Adams <gunnell.karlee@gmail.com>
2016-04-12 10:40:42 -0600

    Fibonacci App for iPhone 5

--- FibonacciApp/ViewController.swift
+++ FibonacciApp/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var fibonacciSequence = FibonacciSequence(numberOfItemsInSequence: 2, includesZero: true)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.updateFibonacciSequence()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var textView: UITextView!
 
+    @IBOutlet weak var includesZeroSwitch: UISwitch!
+    
+    @IBOutlet weak var includesZeroLabel: UILabel!
+    
+    @IBOutlet weak var numberOfItemsLabel: UILabel!
+    
+    @IBOutlet weak var numberOfItemsSlider: UISlider!
+    
+    @IBAction func updateFibonacciSequence() {
+        fibonacciSequence = FibonacciSequence(numberOfItemsInSequence: UInt(numberOfItemsSlider.value), includesZero: includesZeroSwitch.on)
+        if includesZeroSwitch.on {
+            numberOfItemsSlider.maximumValue = 94
+        } else {
+            numberOfItemsSlider.maximumValue = 93
+        }
+        textView.text = fibonacciSequence.values.description
+        includesZeroLabel.text = includesZeroSwitch.on ? "Yes" : "No"
+        numberOfItemsLabel.text = String(Int(numberOfItemsSlider.value))
+    }
+    
 }
 
